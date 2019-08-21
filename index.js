a7d468e39d01834e513cf6fe09af5ebe1c32eaeb
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-08-21 09:14:50 -0400

    Adds PUT, DELETE endpoints

--- index.js
+++ index.js
@@ -37,6 +37,65 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      user
+        ? res.json({ user })
+        : res.status(400).json({
+            error: "User not found"
+          });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(user => {
+      user
+        ? res.json({ user })
+        : res.status(400).json({
+            error: "User not found"
+          });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const newUser = req.body;
+  // console.log(newUser);
+  if (newUser.name && newUser.bio) {
+    db.update(id, newUser)
+      .then(user => {
+        user
+          ? res.status(200).json({ user })
+          : res.status(400).json({
+              error: "User not found"
+            });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "The user information could not be modified."
+        });
+      });
+  } else {
+    res.status(404).json({
+      error: "Please provide name and bio for the user."
+    });
+  }
+});
+
 server.listen(4000, () => {
   console.log("App listening in port 4000");
 });
